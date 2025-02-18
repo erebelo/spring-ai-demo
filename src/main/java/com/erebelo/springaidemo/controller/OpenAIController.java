75a4a8ee8ce35d2373fda0bb9878a17dd9b4f1d0
erebelo <eduldrebelo@gmail.com>
2025-02-18 16:30:07 -0300

    Replacing ResponseEntity with ResponseStatus annotation

--- src/main/java/com/erebelo/springaidemo/controller/OpenAIController.java
+++ src/main/java/com/erebelo/springaidemo/controller/OpenAIController.java
@@ -11,11 +11,12 @@ import com.erebelo.springaidemo.model.Question;
 import com.erebelo.springaidemo.service.OpenAIService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -26,21 +27,24 @@ public class OpenAIController {
 
     private final OpenAIService service;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping(path = OPENAI_ASK_PATH, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Answer> askQuestions(@RequestBody Question question) {
+    public Answer askQuestions(@RequestBody Question question) {
         log.info("GET {}", OPENAI_PATH + OPENAI_ASK_PATH);
-        return ResponseEntity.ok(service.getAnswer(question));
+        return service.getAnswer(question);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping(path = OPENAI_CAPITAL_PATH, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Answer> getCapital(@RequestBody CapitalRequest capitalRequest) {
+    public Answer getCapital(@RequestBody CapitalRequest capitalRequest) {
         log.info("GET {}", OPENAI_PATH + OPENAI_CAPITAL_PATH);
-        return ResponseEntity.ok(service.getCapital(capitalRequest));
+        return service.getCapital(capitalRequest);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping(path = OPENAI_CAPITAL_INFO_PATH, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Answer> getCapitalInfo(@RequestBody CapitalRequest capitalRequest) {
+    public Answer getCapitalInfo(@RequestBody CapitalRequest capitalRequest) {
         log.info("GET {}", OPENAI_PATH + OPENAI_CAPITAL_INFO_PATH);
-        return ResponseEntity.ok(service.getCapitalInfo(capitalRequest));
+        return service.getCapitalInfo(capitalRequest);
     }
 }
